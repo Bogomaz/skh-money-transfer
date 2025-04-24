5cd39b1792b08bae6c2eb4a75e61e45428154be8
Хомякова Светлана <C:\Users\khomyakova\AppData\Roaming\The Bat!>
2025-04-24 16:20:34 +0300

    Corrected calculation of commission for MasterCard

--- src/main/kotlin/MoneyTransfer.kt
+++ src/main/kotlin/MoneyTransfer.kt
@@ -12,41 +12,48 @@ const val MASTERCARD_COMMISSION_RATE = 0.006
 const val MASTERCARD_FIXED_FEE = 20.0
 const val MIN_VISA_COMMISSION = 35.0
 
-
 fun main() {
-
     // Тест 1: VISA, в пределах лимитов
-    val commission1 = calculateCommission(VISA, 500_000.0, 100_000.0, 10_000.0)
+    val commission1 = calculateCommission(cardType = VISA, amountTransfer = 10_000.0)
     println("Тест 1 (VISA, в пределах лимитов): Комиссия от суммы 10 000.0 руб. составит $commission1 руб.\n")
 
     // Тест 2: MASTERCARD, в пределах лимитов
-    val commission2 = calculateCommission(MASTERCARD, 70_000.0, 50_000.0, 20_000.0)
+    val commission2 = calculateCommission(cardType = MASTERCARD, amountTransfer = 20_000.0)
     println("Тест 2 (MASTERCARD, в пределах лимитов): Комиссия от суммы 20 000.0 руб. составит $commission2 руб.\n")
 
     // Тест 3: MIR, в пределах лимитов
-    val commission3 = calculateCommission(MIR, 600_000.0, 150_000.0, 5_000.0)
+    val commission3 = calculateCommission(cardType = MIR, amountTransfer = 5_000.0)
     println("Тест 3 (MIR, в пределах лимитов): Комиссия от суммы 5 000.0 руб. составит $commission3 руб.\n")
 
     // Тест 4: VISA, превышение дневного лимита
-    val commission4 = calculateCommission(VISA, 500_000.0, 200_000.0, 10_000.0)
+    val commission4 = calculateCommission(cardType = VISA, amountTransfer = 10_000.0)
     println("Тест 4 (VISA, превышение дневного лимита): Комиссия от суммы 10 000.0 руб. составит $commission4 руб.\n")
 
     // Тест 5: MASTERCARD, превышение месячного лимита
-    val commission5 = calculateCommission(MASTERCARD, 650_000.0, 50_000.0, 20_000.0)
+    val commission5 = calculateCommission(cardType = MASTERCARD, amountTransfer = 20_000.0)
     println("Тест 5 (MASTERCARD, превышение месячного лимита): Комиссия от суммы 20 000.0 руб. составит $commission5 руб.\n")
 
     // Тест 6: VISA, превышение месячного лимита
-    val commission6 = calculateCommission(VISA, 650_000.0, 100_000.0, 10_000.0)
+    val commission6 = calculateCommission(cardType = VISA, amountTransfer = 10_000.0)
     println("Тест 6 (VISA, превышение месячного лимита): Комиссия от суммы 10 000.0 руб. составит $commission6 руб.\n")
 
     // Тест 7: Неизвестный тип карты
-    val commission7 = calculateCommission(99, 500_000.0, 100_000.0, 10_000.0)
-    println("Тест 7 (неизвестный тип карты): Комиссия от суммы 10 000.0 руб. составит $commission7 руб.")
+    val commission7 = calculateCommission(cardType = 99, amountTransfer = 10_000.0)
+    println("Тест 7 (неизвестный тип карты): Комиссия от суммы 10 000.0 руб. составит $commission7 руб.\n")
+
+    // Тест 8: MASTERCARD, в пределах лимитов, но с превышением льготной суммы
+    val commission8 = calculateCommission(cardType = MASTERCARD, amountTransfer = 150_000.0)
+    println("Тест 8 (MASTERCARD, в пределах лимитов, но с превышением льготной суммы): Комиссия от суммы 150 000.0 руб. составит $commission8 руб.\n")
+
 }
 
 
 // вычисление комиссии
-fun calculateCommission(cardType: Int, amountPerMonth: Double, amountPerDay: Double, amountTransfer: Double): Double {
+fun calculateCommission(
+    cardType: Int = MIR,
+    amountPerMonth: Double = MAX_MONTHLY_LIMIT,
+    amountPerDay: Double = MAX_DAILY_LIMIT,
+    amountTransfer: Double): Double {
     // сначала проверяем, не превысили ли мы лимиты. Если превысили, то комиссии не будет.
     // во время проверки функция isWithinLimits выведет соответствующие сообщения в консоль.
     if (!isWithinLimits(amountPerDay, amountPerMonth)) {
@@ -55,7 +62,7 @@ fun calculateCommission(cardType: Int, amountPerMonth: Double, amountPerDay: Dou
 
     return when (cardType) {
         VISA -> calculateVisaCommission(amountTransfer)
-        MASTERCARD -> calculateMastercardCommission(amountPerMonth, amountTransfer)
+        MASTERCARD -> calculateMastercardCommission(amountTransfer = amountTransfer)
         MIR -> 0.0
         else -> {
             println("Неизвестный тип карты.")
@@ -88,7 +95,8 @@ fun calculateVisaCommission(amountTransfer: Double): Double {
 }
 
 // Функция вычисляет комиссию для карты mastercard
-fun calculateMastercardCommission(amountPerMonth: Double, amountTransfer: Double): Double {
-    val preCalculation = amountTransfer * MASTERCARD_COMMISSION_RATE + MASTERCARD_FIXED_FEE
-    return if (amountPerMonth > MASTERCARD_MONTHLY_LIMIT) preCalculation else 0.0
+fun calculateMastercardCommission(comissionFreeLimit: Double = MASTERCARD_MONTHLY_LIMIT, amountTransfer: Double): Double {
+    return if (amountTransfer - comissionFreeLimit > 0)
+        (amountTransfer - comissionFreeLimit) * MASTERCARD_COMMISSION_RATE + MASTERCARD_FIXED_FEE
+        else 0.0
 }
